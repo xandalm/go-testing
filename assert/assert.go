cf4e9997ad65ac66016a59ec0dfecc32e815e946
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2024-09-13 18:07:20 -0300

    Assertion helpers

---
+++ assert/assert.go
@@ -0,0 +1,320 @@
+package assert
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func output(t testing.TB, common string, out []any) {
+	if len(out) == 0 {
+		t.Fatal(common)
+		return
+	}
+	if str, ok := out[0].(string); ok {
+		t.Fatalf(str, out[1:]...)
+		return
+	}
+	panic("assert: output argument must be a fmt string")
+}
+
+func isNil(v any) bool {
+	if v == nil {
+		return true
+	}
+
+	val := reflect.ValueOf(v)
+	switch val.Kind() {
+	case reflect.Chan, reflect.Func,
+		reflect.Interface, reflect.Pointer, reflect.UnsafePointer,
+		reflect.Map, reflect.Slice:
+		return val.IsNil()
+	default:
+		return false
+	}
+}
+
+func Nil(t testing.TB, v any, out ...any) {
+	t.Helper()
+	if !isNil(v) {
+		output(t, "expected nil value", out)
+	}
+}
+
+func NotNil(t testing.TB, v any, out ...any) {
+	t.Helper()
+	if isNil(v) {
+		output(t, "expected not nil value", out)
+	}
+}
+
+func Zero[T any](t testing.TB, v T, out ...any) {
+	t.Helper()
+
+	if !reflect.ValueOf(v).IsZero() {
+		common := fmt.Sprintf("expected zero value for the type %T", v)
+		output(t, common, out)
+	}
+}
+
+func NotZero[T any](t testing.TB, v T, out ...any) {
+	t.Helper()
+
+	if reflect.ValueOf(v).IsZero() {
+		common := fmt.Sprintf("expected non-zero value for the type %T", v)
+		output(t, common, out)
+	}
+}
+
+func isEmpty(v any) bool {
+	if isNil(v) {
+		return true
+	}
+
+	value := reflect.ValueOf(v)
+
+	switch value.Kind() {
+	case reflect.Array,
+		reflect.Chan,
+		reflect.Map,
+		reflect.Slice,
+		reflect.String:
+		return value.Len() == 0
+	case reflect.Ptr, reflect.UnsafePointer:
+		return isEmpty(value.Elem().Interface())
+	default:
+		zero := reflect.Zero(value.Type())
+		return reflect.DeepEqual(value, zero)
+	}
+}
+
+func Empty[T any](t testing.TB, v T, out ...any) {
+	t.Helper()
+
+	if !isEmpty(v) {
+		common := fmt.Sprintf("expected empty, but got %v", v)
+		output(t, common, out)
+	}
+}
+
+func NotEmpty[T any](t testing.TB, v T, out ...any) {
+	t.Helper()
+
+	if isEmpty(v) {
+		common := fmt.Sprintf("expected not empty, but got %v", v)
+		output(t, common, out)
+	}
+}
+
+func True(t testing.TB, got bool, out ...any) {
+	t.Helper()
+
+	if !got {
+		common := "didn't get true"
+		output(t, common, out)
+	}
+}
+
+func False(t testing.TB, got bool, out ...any) {
+	t.Helper()
+
+	if got {
+		common := "didn't get false"
+		output(t, common, out)
+	}
+}
+
+func Equal[T any](t testing.TB, a, b T, out ...any) {
+	t.Helper()
+
+	if !reflect.DeepEqual(a, b) {
+		common := fmt.Sprintf("want equal values, got %v and want %v", a, b)
+		output(t, common, out)
+	}
+}
+
+func NotEqual[T any](t testing.TB, a, b T, out ...any) {
+	t.Helper()
+
+	if reflect.DeepEqual(a, b) {
+		common := fmt.Sprintf("don't want equal values, got %v and %v", a, b)
+		output(t, common, out)
+	}
+}
+
+func EqualFunc[T any](t testing.TB, a, b T, cmp func(T, T) bool, out ...any) {
+	t.Helper()
+
+	if !cmp(a, b) {
+		common := fmt.Sprintf("want equal values, got %v and want %v", a, b)
+		output(t, common, out)
+	}
+}
+
+func NotEqualFunc[T any](t testing.TB, a, b T, cmp func(T, T) bool, out ...any) {
+	t.Helper()
+
+	if cmp(a, b) {
+		common := fmt.Sprintf("don't want equal values, got %v and %v", a, b)
+		output(t, common, out)
+	}
+}
+
+func Error(t testing.TB, got, want error, out ...any) {
+	t.Helper()
+
+	if got != want {
+		common := fmt.Sprintf("expected error %v, but didn't get it", want)
+		output(t, common, out)
+	}
+}
+
+func NotError(t testing.TB, got, nwant error, out ...any) {
+	t.Helper()
+
+	if got == nwant {
+		common := fmt.Sprintf("didn't expected error %v, but got it", nwant)
+		output(t, common, out)
+	}
+}
+
+func Contains[T any, S []T](t testing.TB, s S, v T, out ...any) {
+	t.Helper()
+
+	for _, e := range s {
+		if reflect.DeepEqual(e, v) {
+			return
+		}
+	}
+	common := fmt.Sprintf("there's no %v in %v", v, s)
+	output(t, common, out)
+}
+
+func NotContains[T any, S []T](t testing.TB, s S, v T, out ...any) {
+	t.Helper()
+
+	for _, e := range s {
+		if reflect.DeepEqual(e, v) {
+			common := fmt.Sprintf("there's %v in %v", v, s)
+			output(t, common, out)
+			return
+		}
+	}
+}
+
+func ContainsFunc[T any, S []T](t testing.TB, s S, v T, fn func(T, T) bool, out ...any) {
+	t.Helper()
+
+	for _, e := range s {
+		if fn(e, v) {
+			return
+		}
+	}
+	common := fmt.Sprintf("there's no %v in %v", v, s)
+	output(t, common, out)
+}
+
+func NotContainsFunc[T any, S []T](t testing.TB, s S, v T, fn func(T, T) bool, out ...any) {
+	t.Helper()
+
+	for _, e := range s {
+		if fn(e, v) {
+			common := fmt.Sprintf("there's %v in %v", v, s)
+			output(t, common, out)
+			return
+		}
+	}
+}
+
+func HasPrefix(t testing.TB, s string, pfx string, out ...any) {
+	t.Helper()
+
+	if !strings.HasPrefix(s, pfx) {
+		common := fmt.Sprintf("the %q cannot be a prefix of the %q", pfx, s)
+		output(t, common, out)
+	}
+}
+
+func HasNoPrefix(t testing.TB, s string, pfx string, out ...any) {
+	t.Helper()
+
+	if strings.HasPrefix(s, pfx) {
+		common := fmt.Sprintf("the %q can be a prefix of the %q", pfx, s)
+		output(t, common, out)
+	}
+}
+
+func HasSuffix(t testing.TB, s string, sfx string, out ...any) {
+	t.Helper()
+
+	if !strings.HasSuffix(s, sfx) {
+		common := fmt.Sprintf("the %q cannot be a suffix of the %q", sfx, s)
+		output(t, common, out)
+	}
+}
+
+func HasNoSuffix(t testing.TB, s string, sfx string, out ...any) {
+	t.Helper()
+
+	if strings.HasSuffix(s, sfx) {
+		common := fmt.Sprintf("the %q can be a suffix of the %q", sfx, s)
+		output(t, common, out)
+	}
+}
+
+func HasSubString(t testing.TB, s string, sub string, out ...any) {
+	t.Helper()
+
+	if !strings.Contains(s, sub) {
+		common := fmt.Sprintf("the %q cannot be a substring of the %q", sub, s)
+		output(t, common, out)
+	}
+}
+
+func HasNoSubString(t testing.TB, s string, sub string, out ...any) {
+	t.Helper()
+
+	if strings.Contains(s, sub) {
+		common := fmt.Sprintf("the %q can be a substring of the %q", sub, s)
+		output(t, common, out)
+	}
+}
+
+func Panics(t testing.TB, fn func(), out ...any) {
+	t.Helper()
+
+	defer func() {
+		if recover() == nil {
+			output(t, "didn't panic", out)
+		}
+	}()
+
+	fn()
+}
+
+func NotPanics(t testing.TB, fn func(), out ...any) {
+	t.Helper()
+
+	defer func() {
+		if r := recover(); r != nil {
+			common := fmt.Sprintf("did panic, %v", r)
+			output(t, common, out)
+		}
+	}()
+
+	fn()
+}
+
+func PanicIs(t testing.TB, fn func(), exp any, out ...any) {
+	t.Helper()
+
+	defer func() {
+		if r := recover(); r != exp {
+			common := fmt.Sprintf("can't get the expected panic, got %v", r)
+			output(t, common, out)
+		}
+	}()
+
+	fn()
+}
---
+++ assert/assert_test.go
@@ -0,0 +1,485 @@
+package assert_test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/xandalm/go-testing/assert"
+)
+
+type tester struct {
+	*testing.T
+	interfered bool
+}
+
+func (t *tester) Fatal(args ...any) {
+	t.interfered = true
+}
+
+func (t *tester) Fatalf(message string, args ...any) {
+	t.interfered = true
+}
+
+var errFoo = errors.New("error")
+
+func assertSuccess(t *testing.T, name string, fn func(t testing.TB)) {
+	t.Helper()
+	t.Run(name, func(t *testing.T) {
+		t.Helper()
+
+		tt := &tester{T: t}
+		fn(tt)
+		if tt.interfered {
+			t.Fatal("shouldn't fail")
+		}
+	})
+}
+
+func assertFailure(t *testing.T, name string, fn func(t testing.TB)) {
+	t.Helper()
+	t.Run(name, func(t *testing.T) {
+		t.Helper()
+
+		tt := &tester{T: t}
+		fn(tt)
+		if !tt.interfered {
+			t.Fatal("should fail")
+		}
+	})
+}
+
+func TestNil(t *testing.T) {
+	var ptr *int
+	var err error
+	var fun func(v int)
+	assertSuccess(t, "nil pointer", func(t testing.TB) {
+		assert.Nil(t, ptr)
+	})
+	assertSuccess(t, "nil error", func(t testing.TB) {
+		assert.Nil(t, err)
+	})
+	assertSuccess(t, "nil func", func(t testing.TB) {
+		assert.Nil(t, fun)
+	})
+	ptr = new(int)
+	err = errFoo
+	fun = func(v int) {}
+	assertFailure(t, "not nil pointer", func(t testing.TB) {
+		assert.Nil(t, ptr)
+	})
+	assertFailure(t, "not nil error", func(t testing.TB) {
+		assert.Nil(t, err)
+	})
+	assertFailure(t, "not nil func", func(t testing.TB) {
+		assert.Nil(t, fun)
+	})
+}
+
+func TestNotNil(t *testing.T) {
+	var ptr *int
+	var err error
+	var fun func(v int)
+	assertFailure(t, "nil pointer", func(t testing.TB) {
+		assert.NotNil(t, ptr)
+	})
+	assertFailure(t, "nil error", func(t testing.TB) {
+		assert.NotNil(t, err)
+	})
+	assertFailure(t, "nil func", func(t testing.TB) {
+		assert.NotNil(t, fun)
+	})
+	ptr = new(int)
+	err = errFoo
+	fun = func(v int) {}
+	assertSuccess(t, "not nil pointer", func(t testing.TB) {
+		assert.NotNil(t, ptr)
+	})
+	assertSuccess(t, "not nil error", func(t testing.TB) {
+		assert.NotNil(t, err)
+	})
+}
+
+func TestZero(t *testing.T) {
+	assertSuccess(t, "zero int", func(t testing.TB) {
+		assert.Zero(t, 0)
+	})
+	assertSuccess(t, "zero string", func(t testing.TB) {
+		assert.Zero(t, "")
+	})
+	assertSuccess(t, "zero struct", func(t testing.TB) {
+		assert.Zero(t, struct{ v any }{})
+	})
+	assertFailure(t, "not zero int", func(t testing.TB) {
+		assert.Zero(t, 1)
+	})
+	assertFailure(t, "not zero string", func(t testing.TB) {
+		assert.Zero(t, "foo")
+	})
+	assertFailure(t, "not zero struct", func(t testing.TB) {
+		assert.Zero(t, struct{ v any }{"a"})
+	})
+}
+
+func TestNotZero(t *testing.T) {
+	assertSuccess(t, "not zero int", func(t testing.TB) {
+		assert.NotZero(t, 1)
+	})
+	assertSuccess(t, "not zero string", func(t testing.TB) {
+		assert.NotZero(t, "foo")
+	})
+	assertSuccess(t, "not zero struct", func(t testing.TB) {
+		assert.NotZero(t, struct{ v any }{"a"})
+	})
+	assertFailure(t, "zero int", func(t testing.TB) {
+		assert.NotZero(t, 0)
+	})
+	assertFailure(t, "zero string", func(t testing.TB) {
+		assert.NotZero(t, "")
+	})
+	assertFailure(t, "zero struct", func(t testing.TB) {
+		assert.NotZero(t, struct{ v any }{})
+	})
+}
+
+func TestEmpty(t *testing.T) {
+	assertSuccess(t, "empty string", func(t testing.TB) {
+		assert.Empty(t, "")
+	})
+	assertSuccess(t, "empty slice", func(t testing.TB) {
+		assert.Empty(t, []any{})
+	})
+	assertSuccess(t, "empty map", func(t testing.TB) {
+		assert.Empty(t, map[any]any{})
+	})
+	assertSuccess(t, "empty chan", func(t testing.TB) {
+		assert.Empty(t, make(chan int, 1))
+	})
+	assertFailure(t, "not empty string", func(t testing.TB) {
+		assert.Empty(t, "foo")
+	})
+	assertFailure(t, "not empty slice", func(t testing.TB) {
+		assert.Empty(t, []any{1})
+	})
+	assertFailure(t, "not empty map", func(t testing.TB) {
+		assert.Empty(t, map[any]any{1: 1})
+	})
+	assertFailure(t, "not empty chan", func(t testing.TB) {
+		ch := make(chan int, 1)
+		ch <- 1
+		assert.Empty(t, ch)
+	})
+}
+
+func TestNotEmpty(t *testing.T) {
+	assertSuccess(t, "not empty string", func(t testing.TB) {
+		assert.NotEmpty(t, "foo")
+	})
+	assertSuccess(t, "not empty slice", func(t testing.TB) {
+		assert.NotEmpty(t, []any{1})
+	})
+	assertSuccess(t, "not empty map", func(t testing.TB) {
+		assert.NotEmpty(t, map[any]any{1: 1})
+	})
+	assertSuccess(t, "not empty chan", func(t testing.TB) {
+		ch := make(chan int, 1)
+		ch <- 1
+		assert.NotEmpty(t, ch)
+	})
+	assertFailure(t, "empty string", func(t testing.TB) {
+		assert.NotEmpty(t, "")
+	})
+	assertFailure(t, "empty slice", func(t testing.TB) {
+		assert.NotEmpty(t, []any{})
+	})
+	assertFailure(t, "empty map", func(t testing.TB) {
+		assert.NotEmpty(t, map[any]any{})
+	})
+	assertFailure(t, "empty chan", func(t testing.TB) {
+		assert.NotEmpty(t, make(chan int, 1))
+	})
+}
+
+func TestTrue(t *testing.T) {
+	assertSuccess(t, "true", func(t testing.TB) {
+		assert.True(t, true)
+	})
+	assertFailure(t, "not true", func(t testing.TB) {
+		assert.True(t, false)
+	})
+}
+
+func TestFalse(t *testing.T) {
+	assertSuccess(t, "false", func(t testing.TB) {
+		assert.False(t, false)
+	})
+	assertFailure(t, "not false", func(t testing.TB) {
+		assert.False(t, true)
+	})
+}
+
+type pointer struct {
+	X, Y float64
+}
+
+func TestEqual(t *testing.T) {
+	assertSuccess(t, "equal numbers", func(t testing.TB) {
+		assert.Equal(t, 1, 1)
+	})
+	assertSuccess(t, "equal strings", func(t testing.TB) {
+		assert.Equal(t, "foo", "foo")
+	})
+	assertSuccess(t, "equal bytes slice", func(t testing.TB) {
+		assert.Equal(t, []byte{'a', 'b'}, []byte{'a', 'b'})
+	})
+	assertSuccess(t, "equal empty bytes slice", func(t testing.TB) {
+		assert.Equal(t, []byte(""), []byte{})
+	})
+	assertSuccess(t, "equal structs", func(t testing.TB) {
+		assert.Equal(t, pointer{0.55, 0.75}, pointer{0.55, 0.75})
+	})
+	assertFailure(t, "nonequal numbers", func(t testing.TB) {
+		assert.Equal(t, 1, 2)
+	})
+	assertFailure(t, "nonequal strings", func(t testing.TB) {
+		assert.Equal(t, "foo", "bar")
+	})
+	assertFailure(t, "nonequal bytes slice", func(t testing.TB) {
+		assert.Equal(t, []byte{'a', 'b'}, []byte{'a', 'c'})
+	})
+	assertFailure(t, "nonequal structs", func(t testing.TB) {
+		assert.Equal(t, pointer{0.55, 0.75}, pointer{1, 1})
+	})
+}
+
+func TestNotEqual(t *testing.T) {
+	assertSuccess(t, "nonequal numbers", func(t testing.TB) {
+		assert.NotEqual(t, 1, 2)
+	})
+	assertSuccess(t, "nonequal strings", func(t testing.TB) {
+		assert.NotEqual(t, "foo", "bar")
+	})
+	assertSuccess(t, "nonequal bytes slice", func(t testing.TB) {
+		assert.NotEqual(t, []byte{'a', 'b'}, []byte{'a', 'c'})
+	})
+	assertSuccess(t, "nonequal structs", func(t testing.TB) {
+		assert.NotEqual(t, pointer{0.55, 0.75}, pointer{1, 1})
+	})
+	assertFailure(t, "equal numbers", func(t testing.TB) {
+		assert.NotEqual(t, 1, 1)
+	})
+	assertFailure(t, "equal strings", func(t testing.TB) {
+		assert.NotEqual(t, "foo", "foo")
+	})
+	assertFailure(t, "equal bytes slice", func(t testing.TB) {
+		assert.NotEqual(t, []byte{'a', 'b'}, []byte{'a', 'b'})
+	})
+	assertFailure(t, "equal empty bytes slice", func(t testing.TB) {
+		assert.NotEqual(t, []byte(""), []byte{})
+	})
+	assertFailure(t, "equal structs", func(t testing.TB) {
+		assert.NotEqual(t, pointer{0.55, 0.75}, pointer{0.55, 0.75})
+	})
+}
+
+func TestEqualFunc(t *testing.T) {
+	cmpFn := func(a, b int) bool {
+		return a == b
+	}
+	assertSuccess(t, "equal", func(t testing.TB) {
+		assert.EqualFunc(t, 1, 1, cmpFn)
+	})
+	assertFailure(t, "nonequal", func(t testing.TB) {
+		assert.EqualFunc(t, 1, 0, cmpFn)
+	})
+}
+
+func TestNotEqualFunc(t *testing.T) {
+	cmpFn := func(a, b int) bool {
+		return a == b
+	}
+	assertSuccess(t, "equal", func(t testing.TB) {
+		assert.NotEqualFunc(t, 1, 1, cmpFn)
+	})
+	assertFailure(t, "nonequal", func(t testing.TB) {
+		assert.NotEqualFunc(t, 1, 0, cmpFn)
+	})
+}
+
+func TestError(t *testing.T) {
+	var err error = errors.New("error")
+
+	assertSuccess(t, "identical error", func(t testing.TB) {
+		assert.Error(t, err, err)
+	})
+	assertSuccess(t, "both nil errors", func(t testing.TB) {
+		assert.Error(t, nil, nil)
+	})
+	assertFailure(t, "different error", func(t testing.TB) {
+		assert.Error(t, err, errors.New("other error"))
+	})
+	assertFailure(t, "error and nil error", func(t testing.TB) {
+		assert.Error(t, err, nil)
+	})
+}
+
+func TestNotError(t *testing.T) {
+	var err error = errors.New("error")
+
+	assertSuccess(t, "different error", func(t testing.TB) {
+		assert.NotError(t, err, errors.New("other error"))
+	})
+	assertSuccess(t, "error and nil error", func(t testing.TB) {
+		assert.NotError(t, err, nil)
+	})
+	assertFailure(t, "identical error", func(t testing.TB) {
+		assert.NotError(t, err, err)
+	})
+	assertFailure(t, "both nil errors", func(t testing.TB) {
+		assert.NotError(t, nil, nil)
+	})
+}
+
+func TestContains(t *testing.T) {
+	assertSuccess(t, "array contains the element", func(t testing.TB) {
+		assert.Contains(t, []int{1, 2, 3, 4, 5}, 3)
+	})
+	assertFailure(t, "array doesn't contain the element", func(t testing.TB) {
+		assert.Contains(t, []int{1, 2, 3, 4, 5}, 0)
+	})
+}
+
+func TestNotContains(t *testing.T) {
+	assertSuccess(t, "array doesn't contains the element", func(t testing.TB) {
+		assert.NotContains(t, []int{1, 2, 3, 4, 5}, 0)
+	})
+	assertFailure(t, "array contain the element", func(t testing.TB) {
+		assert.NotContains(t, []int{1, 2, 3, 4, 5}, 3)
+	})
+}
+
+func TestContainsFunc(t *testing.T) {
+	cmpFn := func(a int, b int) bool {
+		return a == b
+	}
+	assertSuccess(t, "array contains the element", func(t testing.TB) {
+		assert.ContainsFunc(t, []int{1, 2, 3, 4, 5}, 3, cmpFn)
+	})
+	assertFailure(t, "array doesn't contain the element", func(t testing.TB) {
+		assert.ContainsFunc(t, []int{1, 2, 3, 4, 5}, 0, cmpFn)
+	})
+}
+
+func TestNotContainsFunc(t *testing.T) {
+	cmpFn := func(a int, b int) bool {
+		return a == b
+	}
+	assertSuccess(t, "array doesn't contains the element", func(t testing.TB) {
+		assert.NotContainsFunc(t, []int{1, 2, 3, 4, 5}, 0, cmpFn)
+	})
+	assertFailure(t, "array contain the element", func(t testing.TB) {
+		assert.NotContainsFunc(t, []int{1, 2, 3, 4, 5}, 3, cmpFn)
+	})
+}
+
+func TestHasPrefix(t *testing.T) {
+	assertSuccess(t, "string starts with the given string", func(t testing.TB) {
+		assert.HasPrefix(t, "nice to meet you", "nice")
+	})
+	assertFailure(t, "string doesn't start with the given string", func(t testing.TB) {
+		assert.HasPrefix(t, "nice to meet you", "you")
+	})
+}
+
+func TestHasNoPrefix(t *testing.T) {
+	assertSuccess(t, "string doesn't start with the given string", func(t testing.TB) {
+		assert.HasNoPrefix(t, "nice to meet you", "you")
+	})
+	assertFailure(t, "string starts with the given string", func(t testing.TB) {
+		assert.HasNoPrefix(t, "nice to meet you", "nice")
+	})
+}
+
+func TestHasSuffix(t *testing.T) {
+	assertSuccess(t, "string starts with the given string", func(t testing.TB) {
+		assert.HasSuffix(t, "nice to meet you", "you")
+	})
+	assertFailure(t, "string doesn't start with the given string", func(t testing.TB) {
+		assert.HasSuffix(t, "nice to meet you", "meet")
+	})
+}
+
+func TestHasNoSuffix(t *testing.T) {
+	assertSuccess(t, "string doesn't ends with the given string", func(t testing.TB) {
+		assert.HasNoSuffix(t, "nice to meet you", "nice")
+	})
+	assertFailure(t, "string ends with the given string", func(t testing.TB) {
+		assert.HasNoSuffix(t, "nice to meet you", "you")
+	})
+}
+
+func TestHasSubString(t *testing.T) {
+	assertSuccess(t, "string contains the substring", func(t testing.TB) {
+		assert.HasSubString(t, "nice to meet you", "meet")
+	})
+	assertFailure(t, "string doesn't contain the substring", func(t testing.TB) {
+		assert.HasSubString(t, "nice to meet you", "world")
+	})
+}
+
+func TestHasNoSubString(t *testing.T) {
+	assertSuccess(t, "string doesn't contain the substring", func(t testing.TB) {
+		assert.HasNoSubString(t, "nice to meet you", "buddy")
+	})
+	assertFailure(t, "string contains the substring", func(t testing.TB) {
+		assert.HasNoSubString(t, "nice to meet you", "you")
+	})
+}
+
+type writer struct {
+	b []byte
+}
+
+func (w *writer) Write(p []byte) (n int, err error) {
+	w.b = append(w.b, p...)
+	return len(p), nil
+}
+
+func TestPanics(t *testing.T) {
+	assertSuccess(t, "panics", func(t testing.TB) {
+		assert.Panics(t, func() {
+			panic("panic")
+		})
+	})
+	assertFailure(t, "does not panic", func(t testing.TB) {
+		assert.Panics(t, func() {
+			fmt.Fprint(&writer{[]byte{}}, "yo!")
+		})
+	})
+}
+
+func TestNotPanics(t *testing.T) {
+	assertSuccess(t, "does not panic", func(t testing.TB) {
+		assert.NotPanics(t, func() {
+			fmt.Fprint(&writer{[]byte{}}, "yo!")
+		})
+	})
+	assertFailure(t, "panics", func(t testing.TB) {
+		assert.NotPanics(t, func() {
+			panic("panic")
+		})
+	})
+}
+
+func TestPanicIs(t *testing.T) {
+	assertSuccess(t, "panics the expected panic", func(t testing.TB) {
+		assert.PanicIs(t, func() {
+			panic("panic")
+		}, "panic")
+	})
+	assertFailure(t, "does not panic the expected panic", func(t testing.TB) {
+		assert.PanicIs(t, func() {
+			panic("panic")
+		}, "other panic")
+	})
+}
